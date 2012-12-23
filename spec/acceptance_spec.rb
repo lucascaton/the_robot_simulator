d20dee97bd153ca0280181a3b1315c46b27bfc92
Lucas Caton <lucascaton@gmail.com>
2012-12-23 15:12:08 -0200

    Creating more specs

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -5,6 +5,10 @@ describe 'Acceptance' do
   let(:robot) { Robot.new(tabletop) }
 
   it do
+    expect(robot.execute 'REPORT').to eq('PLACE command has not been executed yet')
+  end
+
+  it do
     robot.execute 'PLACE 0,0,NORTH'
     robot.execute 'MOVE'
     expect(robot.execute 'REPORT').to eq('0,1,NORTH')
@@ -23,5 +27,53 @@ describe 'Acceptance' do
     robot.execute 'LEFT'
     robot.execute 'MOVE'
     expect(robot.execute 'REPORT').to eq('3,3,NORTH')
+  end
+
+  it do
+    robot.execute 'PLACE 2,2,EAST'
+    robot.execute 'LEFT'
+    robot.execute 'MOVE'
+    robot.execute 'LEFT'
+    robot.execute 'MOVE'
+    expect(robot.execute 'REPORT').to eq('1,3,WEST')
+    robot.execute 'RIGHT'
+    robot.execute 'MOVE'
+    robot.execute 'RIGHT'
+    robot.execute 'MOVE'
+    robot.execute 'MOVE'
+    robot.execute 'REPORT'
+    expect(robot.execute 'REPORT').to eq('3,4,EAST')
+  end
+
+  it do
+    robot.execute 'PLACE 0,0,SOUTH'
+    robot.execute 'MOVE'
+    robot.execute 'MOVE'
+    robot.execute 'MOVE'
+    expect(robot.execute 'REPORT').to eq('0,0,SOUTH')
+    robot.execute 'LEFT'
+    robot.execute 'MOVE'
+    robot.execute 'RIGHT'
+    robot.execute 'MOVE'
+    expect(robot.execute 'REPORT').to eq('1,0,SOUTH')
+    robot.execute 'LEFT'
+    robot.execute 'LEFT'
+    expect(robot.execute 'REPORT').to eq('1,0,NORTH')
+    robot.execute 'MOVE'
+    robot.execute 'MOVE'
+    expect(robot.execute 'REPORT').to eq('1,2,NORTH')
+    robot.execute 'PLACE 0,0,SOUTH'
+    expect(robot.execute 'REPORT').to eq('0,0,SOUTH')
+  end
+
+  it do
+    robot.execute 'PLACE 0,0,NORTH'
+    expect(robot.execute 'REPORT').to eq('0,0,NORTH')
+    robot.execute 'PLACE 9,9,NORTH'
+    expect(robot.execute 'REPORT').to eq('0,0,NORTH')
+    robot.execute 'PLACE 0,9,NORTH'
+    expect(robot.execute 'REPORT').to eq('0,0,NORTH')
+    robot.execute 'PLACE 9,0,NORTH'
+    expect(robot.execute 'REPORT').to eq('0,0,NORTH')
   end
 end
